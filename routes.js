2ddd100cfe5b719915e057f73cfb70e087432aff
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-11-05 22:07:56 -0300

    Cria rota post do add-post

--- routes.js
+++ routes.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+const PostController = require('./controllers/PostController.js')
+
 router.get('/', (req, res) => res.render('home'))
 
 router.get('/post', (req, res) => res.render('single-post'))
 
 router.get('/posts/add', (req, res) => res.render('add-post'))
+
+router.post('/posts/add', PostController.store)
 
 module.exports = router
