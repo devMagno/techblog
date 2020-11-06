d93d78696cf7d4fd7f51a9296b98a5760e7a09a7
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-11-05 22:48:54 -0300

    Configura rota store

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,1 +1,11 @@
 const Posts = require('../models/Posts.js')
+
+exports.store = async (req, res) => {
+  const post = await Posts.create({
+    title: req.body.title,
+    description: req.body.description,
+    text: req.body.text
+  })
+
+  res.redirect('/')
+}
