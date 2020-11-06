9f775becd712519fbecb15c429e008bd7112e7f3
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-11-05 21:34:28 -0300

    Adiciona rota para add-post

--- routes.js
+++ routes.js
@@ -5,4 +5,6 @@ router.get('/', (req, res) => res.render('home'))
 
 router.get('/post', (req, res) => res.render('single-post'))
 
+router.get('/posts/add', (req, res) => res.render('add-post'))
+
 module.exports = router
